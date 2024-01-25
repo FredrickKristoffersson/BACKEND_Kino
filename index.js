a9d3b30dbab7d0ff2253f2665e4cb739c6c0c66a
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-01-25 15:18:36 +0100

    Added files and simple code/text to files

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import app from "./src/app.js";
+// import app from "./src/app.js";
+
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+app.engine("handlebars", engine());
 
 app.listen(5080);
--- src/app.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { engine } from "express-handlebars";
-
-const app = express();
-
-app.engine("handlebars", engine());
