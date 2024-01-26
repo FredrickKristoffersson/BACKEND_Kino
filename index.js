44881645914fe64bc351c27ab22111356981c656
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-01-26 15:44:32 +0100

    Added files and style to mainpage with handlebar, layout-main-file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // import app from "./src/app.js";
 
-import express from "express";
+import express, { response } from "express";
 import { engine } from "express-handlebars";
 
 const app = express();
 
 app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+app.get("/", async (request, response) => {
+  response.render("./layouts/main");
+});
+
+app.use("/static", express.static("./static"));
 
 app.listen(5080);
