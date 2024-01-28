a89d6f76f7c137d3c27f41f2e736e704c2e6cc33
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-01-28 17:52:29 +0100

    Created a test with jest, supertest

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// import app from "./src/app.js";
-
-import express, { response } from "express";
-import { engine } from "express-handlebars";
-import { loadMoviesFromAPI, separateMovie } from "./src/movies.js";
-
-const app = express();
-
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./templates");
-
-app.get("/", async (request, response) => {
-  response.render("home");
-});
-
-app.get("/movies", async (request, response) => {
-  const movies = await loadMoviesFromAPI();
-  response.render("movieSection", { movies });
-});
-
-app.get("/movies/:movieId", async (request, response) => {
-  const movie = await separateMovie(request.params.movieId);
-  response.render("singleMovie", { movie });
-});
-
-app.get("/", async (request, response) => {
-  response.render("./layouts/main");
-});
-
-app.use("/static", express.static("./static"));
-app.use("/assets", express.static("./assets"));
+import app from "./src/app.js";
 
 app.listen(5080);
